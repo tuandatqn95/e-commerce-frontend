c46907c4b0b7a061de0b04663fe40c03814161be
IvyNguyen <ivynguyen01000010@gmail.com>
2018-07-22 14:32:44 +0700

    alert add,delete user

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -8,11 +8,11 @@ let reducer = (state = initialState, action) => {
             return [...action.users];
         case Types.ADD_USER:
             state.push(action.user);
-            console.log("ADDED USER");
+            alert("Add user successfully");
             return [...state];
         case Types.DELETE_USER:
             state = state.filter(user => user.id !== action.id);
-            console.log("DELETED USER");
+            alert("Delete user successfully");
             return [...state];
         default:
             return [...state];
